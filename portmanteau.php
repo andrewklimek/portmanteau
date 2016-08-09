007c63b969dd6bfa90992501858e6d283c969914
Andrew Klimek <andrew.klimek@gmail.com>
2016-08-09 01:05:12 +0000

    added defaults and fixed 2 bugs

--- portmanteau.php
+++ portmanteau.php
@@ -25,7 +25,106 @@ if(!function_exists('poo')){function poo($v,$l=''){if(WP_DEBUG_LOG){error_log("*
 register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
 register_activation_hook( __FILE__, 'portmanteau_flush_rewrites' );
 add_action( 'init', 'portmanteau_register_custom_post_types' );
-add_action( 'add_meta_boxes_portmanteau', 'portmanteau_add_custom_box' );
+add_action( 'add_meta_boxes_project', 'portmanteau_add_custom_box' );
+
+/**
+* Shortcode for category listing, used for timeline
+*/
+add_shortcode( 'quickcat', 'quickcat');
+function quickcat($atts){
+	$atts = shortcode_atts( array(
+		'cat' => '',
+		'num' => '16',
+		'order' => 'DESC',
+		'chars' => 80,
+		'type' => 'post'
+	), $atts, 'quickcat' );
+
+	$query = new WP_Query( array( 
+		'category_name' => $atts['cat'], 
+		'posts_per_page' => $atts['num'], 
+		'order' => $atts['order'], 
+		'post_type' => $atts['type']
+	) );	
+	
+	ob_start();
+
+	// The Loop
+	if ( $query->have_posts() ) {
+		echo '<div class="quickcat">';
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			// get_template_part( 'template-parts/content', get_post_format() );
+			?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+	<header class="entry-header">
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+	</header>
+	<div class="entry-content">
+		<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail();
+			}
+			
+			if ( $excerpt = get_the_excerpt() ) {
+				if ( strlen( $excerpt ) > (int) $atts['chars'] ) {
+					$excerpt = substr( $excerpt, 0, strpos( $excerpt, ' ', (int) $atts['chars'] ) );
+				}
+				print "<p class='quickcat-excerpt'>{$excerpt}…</p>";
+			}
+		?>
+	</div>
+	<span class="readmore">Read More &rarr;</span>
+</a>
+</article>
+			<?php
+		}
+		echo '</div>';
+		the_posts_navigation();
+	}
+	/* Restore original Post Data */
+	wp_reset_postdata();
+
+	return ob_get_clean();
+}
+
+/**
+* Masonry Shortcode
+*/
+add_shortcode('frenchmason', 'frenchpress_masonry' );
+function frenchpress_masonry( $a, $content = '' ) {
+	
+	if ( ! $content ) return "no content in [frenchmason] shortcode";
+	
+	$child = !empty( $a['child'] ) ? ' > :first-child' : '';
+	$selector = !empty( $a['selector'] ) ? $a['selector'] : '#frenchmason > *';
+	if ( empty( $a['width'] ) ) {
+		$width = "'{$selector}'";
+	} elseif ( is_numeric( $a['width'] ) ) {
+		$width = $a['width'];
+	} else {
+		$width = "'{$a['width']}'";
+	}
+	
+	$snippet = "
+	var grid = document.querySelector('#frenchmason{$child}');
+	imagesLoaded( grid, function() {
+		var msnry = new Masonry( grid, {
+			itemSelector: '{$selector}',
+			columnWidth: {$width},
+			percentPosition: true,
+			// gutter: 10
+		});
+	});
+	";
+	wp_enqueue_script( 'masonry' );
+	wp_add_inline_script( 'masonry', $snippet );
+	
+	$out = "<div id='frenchmason' class='frenchmason'>". do_shortcode($content) ."</div>";
+	
+	return $out;
+}
 
 /***
  * Silly shortcode for displaying custom meta
@@ -73,19 +172,22 @@ function portmanteau_inner_custom_box( $post ) {
 	$values = get_post_meta( $post->ID );
 	
 	$fields = array(
-		'website'	=>	'Website',
-		'facebook'	=>	'Facebook',
-		'twitter'	=>	'Twitter',
-		'instagram'	=>	'Instagram',
+		'Website'	=>	'//hillaryraimo.com/',
+		'Facebook'	=>	'https://www.facebook.com/HillaryRaimo/',
+		'Twitter'	=>	'https://twitter.com/hillaryraimo/',
+		'Instagram'	=>	'https://www.instagram.com/hillaryraimo11/',
 	);
 
 	print "<table class='form-table'><tbody>";
 	
-	foreach ( $fields as $field => $label ) {
+	foreach ( $fields as $label => $default ) {
+		$field = strtolower( str_replace( ' ', '_', $label ) );
 		print "
 	<tr>
 		<th scope='row'><label for='portmanteau_{$field}'>{$label}</label></th>
-		<td><input name='meta_input[{$field}]' type='text' id='portmanteau_{$field}' value='{$values[ $field ][0]}' class='regular-text ltr'></td>
+		<td><input name='meta_input[{$field}]' type='text' id='portmanteau_{$field}' value='";
+	print !empty( $values[ $field ][0] ) ? $values[ $field ][0] : $default;
+	print "' class='regular-text ltr'></td>
 	</tr>";
 	}
 
@@ -96,7 +198,7 @@ function portmanteau_inner_custom_box( $post ) {
 
 function portmanteau_register_custom_post_types() {
 
-	register_post_type( 'portmanteau', array(
+	register_post_type( 'project', array(
 		'label'               => 'Projects',
 		'public'              => true,
 		'menu_position'       => 5,
@@ -105,7 +207,7 @@ function portmanteau_register_custom_post_types() {
 		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', ),
 		'taxonomies'          => array( 'category', 'post_tag' ),
 		'has_archive'         => true,
-		'rewrite'            => array( 'slug' => 'projects' ),
+		'rewrite'            => array( 'slug' => 'project' ),
 	) );
 
 }
